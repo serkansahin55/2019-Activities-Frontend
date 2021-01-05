b20461337caef430ea8c9cabe49e918fe1f8d42e
serkansahin55 <ssahin55@outlook.com>
2021-01-05 17:58:57 -0500

    additions

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/* DOM Elements */
+const recipeImage = document.querySelector("re")
+
+/* Render Functions */
+
+/* Fetch Functions */
+const getOneActivity = id => {
+    fetch(`http://localhost:3000/api/v1/activities/${id}`)
+        .then(r => r.json())
+        .then(console.log)
+}
+
+// const getOneActivity = async (id) => {
+//     const url = `http://localhost:3000/api/v1/activities/${id}`
+//     const response = await fetch(url)
+//     const activityObj = await response.json()
+//     console.log(activityObj)
+// }
+
+/* Initialize */
+getOneActivity(32)
