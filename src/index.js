b63041b8ea8c22a6b978323fb9d056d5046fb1c3
tgarayua <tgarayua@gmail.com>
2021-01-06 13:00:01 -0500

    fetched title, image and image alt

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 /* DOM Elements */
+const activityTitle = document.querySelector("#activity-title")
+const activityImage = document.querySelector("#activity-picture")
 //const recipeImage = document.querySelector("re")
 
 /* Render Functions */
@@ -7,15 +9,14 @@
 const getOneActivity = id => {
     fetch(`http://localhost:3000/api/v1/activities/${id}`)
         .then(r => r.json())
-        .then(console.log)
+        .then(data => {
+            console.log(data)
+            activityTitle.innerHTML = data.title
+            activityImage.src = data.image_url
+            activityImage.alt = data.title
+      
+})
 }
 
-// const getOneActivity = async (id) => {
-//     const url = `http://localhost:3000/api/v1/activities/${id}`
-//     const response = await fetch(url)
-//     const activityObj = await response.json()
-//     console.log(activityObj)
-// }
-
 /* Initialize */
-getOneActivity(32)
+getOneActivity(24)
