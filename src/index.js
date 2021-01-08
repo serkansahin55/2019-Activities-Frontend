83028246ec74f5d20cc016d75f8ee32ff00cd4e1
serkansahin55 <ssahin55@outlook.com>
2021-01-07 19:33:29 -0500

    displayactivities

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
-/* DOM Elements */
-const activityTitle = document.querySelector("#activity-title")
-const activityImage = document.querySelector("#activity-picture")
-//const recipeImage = document.querySelector("re")
+// See all activities names in a `div` with the id of `"character-bar"`. On page load, **request** data from the server to get all of the activities objects. When you have this information, you'll need to add a `span` tag with the character's name to the activity bar.
 
-/* Render Functions */
 
-/* Fetch Functions */
-const getOneActivity = id => {
-    fetch(`http://localhost:3000/api/v1/activities/${id}`)
-        .then(r => r.json())
-        .then(data => {
-            console.log(data)
-            activityTitle.innerHTML = data.title
-            activityImage.src = data.image_url
-            activityImage.alt = data.title
-      
-})
+
+
+
+
+const renderImage = activity => {
+    const img = document.createElement("img")
+    img.src = activity.image_url
+    img.alt = activity.title
+    const activityBar = document.querySelector("#activity-bar")
+    img.addEventListener("click", () => {
+        getActivity(activity.id)
+    })
+
+    activityBar.append(img)
 }
 
-/* Initialize */
-getOneActivity(24)
+
+const renderAllActivities = activityArray => {
+    activityArray.forEach(renderImage)
+}
+
+
+const renderDetail = activity => {
+    const img = document.querySelector(".detail-image")
+    img.src = activity.image_url
+    img.alt = activity.title
+
+    const h2 = document.querySelector(".name")
+    h2.textContent = activity.title
+
+}
+
+const getActivity = id => {
+    fetch(`http://localhost:3000/api/v1/activities/${id}`)
+       .then(r => r.json ())
+       .then(renderDetail)
+   }
+
+
+const getActivities = () => {
+ fetch("http://localhost:3000/api/v1/activities")
+    .then(r => r.json ())
+    .then(renderAllActivities)
+}
+
+
+getActivities()
