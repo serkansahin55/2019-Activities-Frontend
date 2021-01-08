07a22e3ad1b78a2bdf9d7904692916bdf7a5c662
tgarayua <tgarayua@gmail.com>
2021-01-08 08:52:24 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@
 const activityTitle = document.querySelector("#activity-title")
 const activityImage = document.querySelector("#activity-picture")
 const activityComment = document.querySelector("#activity-comment")
-//const recipeImage = document.querySelector("re")
+const activitiyCommentId = document.querySelector('#activity-comment-id')
+const newCommentForm = document.querySelector('#new-activity-comment')
+const activityDiv = document.querySelector('.activity-div')
 
-/* Render Functions */
+const commentFormListener = (event) => {
+    event.preventDefault()
+    const commentContent = event.target.content.value
+    const activityId = event.target.parentElement.dataset.id
+    const newComment = {
+        activity_id: activityId,
+        content: commentContent
+    }
+    submitActivityComment(newComment)
+}
+
+newCommentForm.addEventListener('submit', commentFormListener)
+
+const submitActivityComment = (newComment) => {
+    fetch(`http://localhost:3000/api/v1/comments`, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(newComment)
+    })
+    .then(response => response.json())
+    .then(response => activityComment.innerHTML = ())
+}
 
 /* Fetch Functions */
 const getOneActivity = id => {
@@ -15,6 +40,7 @@ const getOneActivity = id => {
             activityTitle.innerHTML = data.title
             activityImage.src = data.image_url
             activityImage.alt = data.title
+            activityDiv.dataset.id = id
             
             data.comments.forEach(element => {
                 const newLi = document.createElement("li")
@@ -26,4 +52,4 @@ const getOneActivity = id => {
     }
 
 /* Initialize */
-getOneActivity(42)
+getOneActivity(24)
